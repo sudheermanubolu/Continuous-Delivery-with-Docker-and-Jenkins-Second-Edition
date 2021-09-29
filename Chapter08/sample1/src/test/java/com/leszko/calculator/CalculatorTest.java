b75edc274b62b2b8ce0382b0a7d2c42f763811cc
sudheermanubolu <44275113+sudheermanubolu@users.noreply.github.com>
2021-09-28 20:57:43 -0700

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -2,7 +2,6 @@ package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-/** test. */
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
